4d94683c220854b398ce1e627949e0fde0953381
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-10-16 00:08:11 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('create-task-form');
+    const taskList = document.getElementById('tasks');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevent form submission
+  
+      const taskDescription = document.getElementById('new-task-description').value;
+  
+      // Create a new list item and add it to the task list
+      const newTask = document.createElement('li');
+      newTask.textContent = taskDescription;
+      taskList.appendChild(newTask);
+  
+      // Clear the input field
+      document.getElementById('new-task-description').value = '';
+    });
+  });
 });
